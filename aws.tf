ad0c4efdec5507bb272e7c1969d3e89618eeefed
jethhin50 <86185471+jethhin50@users.noreply.github.com>
2022-09-29 21:14:55 +0530

    Create aws.tf

---
+++ aws.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "jenkins-demo" {
+    ami = "ami-0f924dc71d44d23e2"
+    instance_type = "t2.micro"
+    count = "1"
+    key_name = "jenkins-key"
+
+    tags = {
+      "Name" =  "jenkins-demo"
+
+    }
+    
+}
